55be2feaa177dcd4a53ca5dc5da6e80630860299
codebyelton <eltonfelii@outlook.com>
2026-06-24 17:06:15 -0300

    feat: adiciona estudos da semana 2 (tipos de dados)

---
+++ mes-01/projetos/miniRpg/target/debug/deps/miniRpg-6997e18b47fde983.d
@@ -0,0 +1,5 @@
+C:\Users\Elton\backend-roadmap\mes-01\projetos\miniRpg\target\debug\deps\miniRpg-6997e18b47fde983.d: src\main.rs
+
+C:\Users\Elton\backend-roadmap\mes-01\projetos\miniRpg\target\debug\deps\libminiRpg-6997e18b47fde983.rmeta: src\main.rs
+
+src\main.rs:
---
+++ mes-01/projetos/miniRpg/target/debug/deps/miniRpg-b76a460db196aed0.d
@@ -0,0 +1,5 @@
+C:\Users\Elton\backend-roadmap\mes-01\projetos\miniRpg\target\debug\deps\miniRpg-b76a460db196aed0.d: src\main.rs
+
+C:\Users\Elton\backend-roadmap\mes-01\projetos\miniRpg\target\debug\deps\libminiRpg-b76a460db196aed0.rmeta: src\main.rs
+
+src\main.rs:
